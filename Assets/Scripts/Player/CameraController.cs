fed5f9037fa3235b3f1bb7e38fd12845b3152616
LeoWPereira <leonardo.wpereira@hotmail.com>
2021-03-08 21:09:47 +0100

    Add material to signboard oak

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float pitch = 2f;
+
+    private float currentZoom = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = (target.position - (offset * currentZoom));
+        transform.LookAt(target.position + Vector3.up * pitch);
+    }
+}
