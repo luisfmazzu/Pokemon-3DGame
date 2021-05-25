e3f26510ed94f73d5e1b0cfe1c3025244fabe6fb
Luis Ortiz <luisfmazzu@hotmail.com>
2021-05-25 15:52:38 -0300

    Fixes player rotate not being smooth

--- Assets/Scripts/Player/PlayerTest.cs
+++ Assets/Scripts/Player/PlayerTest.cs
@@ -39,8 +39,8 @@ public class PlayerTest : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
         }
 
         // Updates the animator
