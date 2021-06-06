de00f6440db068f00c8a4ba297d2bed5ccb40df0
LeoWPereira <leonardo.wpereira@hotmail.com>
2021-06-06 11:00:23 +0200

    Add Pokemon Icons

--- Assets/Scripts/Game UI/Player Info Bar/PlayerInfoPokeballs.cs
+++ Assets/Scripts/Game UI/Player Info Bar/PlayerInfoPokeballs.cs
@@ -6,17 +6,17 @@ public class PlayerInfoPokeballs : MonoBehaviour
     #region Private Variables Declaration
         [SerializeField, Range(1, 6)] private int pokeballIndex;
 
-        private Dictionary<int, float> pokeballPlacementDict = new Dictionary<int, float>(){ {1, 0.26f},
-                                                                                             {2, 0.205f},
-                                                                                             {3, 0.1675f},
-                                                                                             {4, 0.1425f},
-                                                                                             {5, 0.124f},
-                                                                                             {6, 0.11f} };
+        private Dictionary<int, float> pokeballPlacementDict = new Dictionary<int, float>(){ {1, 0.2725f},
+                                                                                             {2, 0.2135f},
+                                                                                             {3, 0.175f},
+                                                                                             {4, 0.1471f},
+                                                                                             {5, 0.127f},
+                                                                                             {6, 0.1123f} };
     #endregion
 
     void Start()
     {
-        RectTransform spriteTransform = this.transform.Find("Sprite").GetComponent<RectTransform>();
+        RectTransform spriteTransform = this.transform.Find("Ball Sprite").GetComponent<RectTransform>();
 
         GUIScreenInfo.ScreenRatio ratio = GUIScreenInfo.Instance.currentToDefaultRatio;
 
@@ -25,7 +25,7 @@ public class PlayerInfoPokeballs : MonoBehaviour
         pokeballPlacementDict.TryGetValue(pokeballIndex, out pokeballPlacementMultiplier);
 
         float widthRatio  = pokeballPlacementMultiplier / ratio.width;
-        float heigthRatio = 0.58f / ratio.heigth;
+        float heigthRatio = 0.61f / ratio.heigth;
 
         spriteTransform.position = new Vector3((spriteTransform.rect.width / widthRatio), Screen.height - (spriteTransform.rect.height / heigthRatio), 0.0f);
     }
