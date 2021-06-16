c33eaf2927040f97f9d1717cc37895fd5495f3d4
LeoWPereira <leonardo.wpereira@hotmail.com>
2021-06-16 16:10:18 +0200

    Add some more pokemon resources
    # Conflicts:
    #	UserSettings/EditorUserSettings.asset

--- Assets/Scripts/Pokemon/PokemonResources.cs
+++ Assets/Scripts/Pokemon/PokemonResources.cs
@@ -47,14 +47,16 @@ public class PokemonResources : MonoBehaviour
 
     public void LoadResources()
     {
-        this.pokemonResource["1_n"]         = new Resource("1", "1_n");
-        this.pokemonResource["1_s"]         = new Resource("1", "1_s");
-        this.pokemonResource["2_n"]         = new Resource("2", "2_n");
-        this.pokemonResource["2_s"]         = new Resource("2", "2_s");
-        this.pokemonResource["25_n"]        = new Resource("25", "25_n");
-        this.pokemonResource["25_s"]        = new Resource("25", "25_s");
-        this.pokemonResource["25f_n"]       = new Resource("25", "25f_n");
-        this.pokemonResource["25f_s"]       = new Resource("25", "25f_s");
+        this.pokemonResource["1_n"]         = new Resource("1", "1_n");                 // Normal Bulbasaur
+        this.pokemonResource["1_s"]         = new Resource("1", "1_s");                 // Shiny Bulbasaur
+        this.pokemonResource["2_n"]         = new Resource("2", "2_n");                 // Normal Ivysaur
+        this.pokemonResource["2_s"]         = new Resource("2", "2_s");                 // Normal Venusaur
+        this.pokemonResource["25_n"]        = new Resource("25", "25_n");               // Normal Male Pikachu
+        this.pokemonResource["25_s"]        = new Resource("25", "25_s");               // Shiny Male Pikachu
+        this.pokemonResource["25f_n"]       = new Resource("25", "25f_n");              // Normal Female Pikachu
+        this.pokemonResource["25f_s"]       = new Resource("25", "25f_s");              // Shiny Female Pikachu
+        this.pokemonResource["149_n"]       = new Resource("149", "149_n");             // Normal Dragonite
+        this.pokemonResource["149_s"]       = new Resource("149", "149_s");             // Shiny Dragonite
 
         this.ready = true;
     }
