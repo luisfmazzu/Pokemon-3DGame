323d85f74723e7352046b4a201086c6f15020ff5
LeoWPereira <leonardo.wpereira@hotmail.com>
2021-07-07 23:55:36 +0200

    Update PokeInfo stats

--- Assets/Resources/GUI/PokeInfo/Scripts/PokeInfoGUI.cs
+++ Assets/Resources/GUI/PokeInfo/Scripts/PokeInfoGUI.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PokeInfoGUI : MonoBehaviour
@@ -140,7 +142,7 @@ public abstract class GenericInfoGUI
         this.mainPanel = coreTransform.Find("Panel").gameObject;
 
         this.button = coreTransform.Find("Button").GetComponent<Button>();
-        button.onClick.AddListener(TaskOnClick);
+        button.onClick.AddListener(PanelTaskOnClick);
 
         this.Awake();
         this.Start();
@@ -152,7 +154,7 @@ public abstract class GenericInfoGUI
 
     public abstract void Update();
 
-    public abstract void TaskOnClick();
+    public abstract void PanelTaskOnClick();
 
     public void DisablePanel()
     {
@@ -207,7 +209,6 @@ public class InfoPanel : GenericInfoGUI
 
     public override void Start()
     {
-        this.hpBar.MaxIntBarValue           = this.pokemon.stats.maxHP;
         this.happinessBar.MaxIntBarValue    = BaseAttributes.MAX_HAPPINESS_VALUE;
 
         this.natureText.text    = SystemManager.Instance.PokemonData.pokemonNatures.RetrievePokemonNature(this.pokemon.natureID).name;
@@ -219,6 +220,7 @@ public class InfoPanel : GenericInfoGUI
 
     public override void Update()
     {
+        this.UpdateStats();
         this.UpdateBars();
     }
 
@@ -285,11 +287,12 @@ public class InfoPanel : GenericInfoGUI
 
     public void UpdateBars()
     {
+        this.hpBar.MaxIntBarValue   = this.pokemon.stats.maxHP;
         this.hpBar.BarValue         = (this.pokemon.stats.currentHP / (float)this.pokemon.stats.maxHP) * 100.0f;
         this.happinessBar.BarValue  = (this.pokemon.happinness / (float)BaseAttributes.MAX_HAPPINESS_VALUE) * 100.0f;
     }
 
-    public override void TaskOnClick()
+    public override void PanelTaskOnClick()
     {
         this.rootGUI.UpdateState(PokeInfoGUI.FSM.Info);
     }
@@ -317,7 +320,7 @@ public class MovesPanel : GenericInfoGUI
 
     }
 
-    public override void TaskOnClick()
+    public override void PanelTaskOnClick()
     {
         this.rootGUI.UpdateState(PokeInfoGUI.FSM.Moves);
     }
@@ -326,6 +329,31 @@ public class MovesPanel : GenericInfoGUI
 public class IVEVPanel : GenericInfoGUI
 {
     #region Private Variables Declaration
+        private Text hpIV;
+        private Text attackIV;
+        private Text defenseIV;
+        private Text spAttackIV;
+        private Text spDefenseIV;
+        private Text speedIV;
+        private Text hpTrainedEV;
+        private Text attackTrainedEV;
+        private Text defenseTrainedEV;
+        private Text spAttackTrainedEV;
+        private Text spDefenseTrainedEV;
+        private Text speedTrainedEV;
+        private Text hpAvailableEV;
+        private Text attackAvailableEV;
+        private Text defenseAvailableEV;
+        private Text spAttackAvailableEV;
+        private Text spDefenseAvailableEV;
+        private Text speedAvailableEV;
+
+        private Button  hpEVButton;
+        private Button  hpAttackButton;
+        private Button  hpDefenseButton;
+        private Button  hpSpAttackButton;
+        private Button  hpSpDefenseButton;
+        private Button  hpSpeedButton;
     #endregion
 
     public IVEVPanel(Pokemon _pokemon, Transform _coreTransform, PokeInfoGUI _rootGUI) : base(_pokemon, _coreTransform, _rootGUI)
@@ -334,20 +362,117 @@ public class IVEVPanel : GenericInfoGUI
 
     public override void Awake()
     {
+        Transform statsTransform = this.coreTransform.Find("Panel").Find("Attributes");
+
+        this.hpIV                   = statsTransform.Find("HP").Find("IV").Find("Value").GetComponent<Text>();
+        this.hpTrainedEV            = statsTransform.Find("HP").Find("EV").Find("Trained EV").GetComponent<Text>();
+        this.hpAvailableEV          = statsTransform.Find("HP").Find("EV").Find("Available EV").GetComponent<Text>();
+        this.hpEVButton             = statsTransform.Find("HP").Find("EV").Find("Increment Button").GetComponent<Button>();
+
+        this.attackIV               = statsTransform.Find("Attack").Find("IV").Find("Value").GetComponent<Text>();
+        this.attackTrainedEV        = statsTransform.Find("Attack").Find("EV").Find("Trained EV").GetComponent<Text>();
+        this.attackAvailableEV      = statsTransform.Find("Attack").Find("EV").Find("Available EV").GetComponent<Text>();
+        this.hpAttackButton         = statsTransform.Find("Attack").Find("EV").Find("Increment Button").GetComponent<Button>();
+
+        this.defenseIV              = statsTransform.Find("Defense").Find("IV").Find("Value").GetComponent<Text>();
+        this.defenseTrainedEV       = statsTransform.Find("Defense").Find("EV").Find("Trained EV").GetComponent<Text>();
+        this.defenseAvailableEV     = statsTransform.Find("Defense").Find("EV").Find("Available EV").GetComponent<Text>();
+        this.hpDefenseButton        = statsTransform.Find("Defense").Find("EV").Find("Increment Button").GetComponent<Button>();
+
+        this.spAttackIV             = statsTransform.Find("Sp. Attack").Find("IV").Find("Value").GetComponent<Text>();
+        this.spAttackTrainedEV      = statsTransform.Find("Sp. Attack").Find("EV").Find("Trained EV").GetComponent<Text>();
+        this.spAttackAvailableEV    = statsTransform.Find("Sp. Attack").Find("EV").Find("Available EV").GetComponent<Text>();
+        this.hpSpAttackButton       = statsTransform.Find("Sp. Attack").Find("EV").Find("Increment Button").GetComponent<Button>();
+
+        this.spDefenseIV            = statsTransform.Find("Sp. Defense").Find("IV").Find("Value").GetComponent<Text>();
+        this.spDefenseTrainedEV     = statsTransform.Find("Sp. Defense").Find("EV").Find("Trained EV").GetComponent<Text>();
+        this.spDefenseAvailableEV   = statsTransform.Find("Sp. Defense").Find("EV").Find("Available EV").GetComponent<Text>();
+        this.hpSpDefenseButton      = statsTransform.Find("Sp. Defense").Find("EV").Find("Increment Button").GetComponent<Button>();
+
+        this.speedIV                = statsTransform.Find("Speed").Find("IV").Find("Value").GetComponent<Text>();
+        this.speedTrainedEV         = statsTransform.Find("Speed").Find("EV").Find("Trained EV").GetComponent<Text>();
+        this.speedAvailableEV       = statsTransform.Find("Speed").Find("EV").Find("Available EV").GetComponent<Text>();
+        this.hpSpeedButton          = statsTransform.Find("Speed").Find("EV").Find("Increment Button").GetComponent<Button>();
+
+        this.hpEVButton.onClick.AddListener(HPIncrementOnClick);
+        this.hpAttackButton.onClick.AddListener(AttackIncrementOnClick);
+        this.hpDefenseButton.onClick.AddListener(DefenseIncrementOnClick);
+        this.hpSpAttackButton.onClick.AddListener(SpAttackIncrementOnClick);
+        this.hpSpDefenseButton.onClick.AddListener(SpDefenseIncrementOnClick);
+        this.hpSpeedButton.onClick.AddListener(SpeedIncrementOnClick);
     }
 
     public override void Start()
     {
+        this.UpdateAttributesIV();
+        this.UpdateAttributesEV();
     }
 
     public override void Update()
     {
-
+        this.UpdateAttributesEV();
     }
 
-    public override void TaskOnClick()
+    void UpdateAttributesIV()
+    {
+        this.hpIV.text          = this.pokemon.ivs.hp.ToString();
+        this.attackIV.text      = this.pokemon.ivs.attack.ToString();
+        this.defenseIV.text     = this.pokemon.ivs.defense.ToString();
+        this.spAttackIV.text    = this.pokemon.ivs.spAttack.ToString();
+        this.spDefenseIV.text   = this.pokemon.ivs.spDefense.ToString();
+        this.speedIV.text       = this.pokemon.ivs.speed.ToString();
+    }
+
+    void UpdateAttributesEV()
+    {
+        this.hpTrainedEV.text           = this.pokemon.trainedEVs.hp.ToString();
+        this.attackTrainedEV.text       = this.pokemon.trainedEVs.attack.ToString();
+        this.defenseTrainedEV.text      = this.pokemon.trainedEVs.defense.ToString();
+        this.spAttackTrainedEV.text     = this.pokemon.trainedEVs.spAttack.ToString();
+        this.spDefenseTrainedEV.text    = this.pokemon.trainedEVs.spDefense.ToString();
+        this.speedTrainedEV.text        = this.pokemon.trainedEVs.speed.ToString();
+
+        this.hpAvailableEV.text         = "(" + this.pokemon.untrainedEVs.hp.ToString() + ")";
+        this.attackAvailableEV.text     = "(" + this.pokemon.untrainedEVs.attack.ToString() + ")";
+        this.defenseAvailableEV.text    = "(" + this.pokemon.untrainedEVs.defense.ToString() + ")";
+        this.spAttackAvailableEV.text   = "(" + this.pokemon.untrainedEVs.spAttack.ToString() + ")";
+        this.spDefenseAvailableEV.text  = "(" + this.pokemon.untrainedEVs.spDefense.ToString() + ")";
+        this.speedAvailableEV.text      = "(" + this.pokemon.untrainedEVs.speed.ToString() + ")";
+    }
+
+    public override void PanelTaskOnClick()
     {
         this.rootGUI.UpdateState(PokeInfoGUI.FSM.IVEV);
+    }
+
+    void HPIncrementOnClick()
+    {
+        this.pokemon.MoveUntrainedEVToTrainedEV(PokemonEnums.Stats.hp, 1);
+    }
+
+    void AttackIncrementOnClick()
+    {
+        this.pokemon.MoveUntrainedEVToTrainedEV(PokemonEnums.Stats.attack, 1);
+    }
+
+    void DefenseIncrementOnClick()
+    {
+        this.pokemon.MoveUntrainedEVToTrainedEV(PokemonEnums.Stats.defense, 1);
+    }
+
+    void SpAttackIncrementOnClick()
+    {
+        this.pokemon.MoveUntrainedEVToTrainedEV(PokemonEnums.Stats.spAttack, 1);
+    }
+
+    void SpDefenseIncrementOnClick()
+    {
+        this.pokemon.MoveUntrainedEVToTrainedEV(PokemonEnums.Stats.spDefense, 1);
+    }
+
+    void SpeedIncrementOnClick()
+    {
+        this.pokemon.MoveUntrainedEVToTrainedEV(PokemonEnums.Stats.speed, 1);
     }
 }
 
@@ -376,7 +501,7 @@ public class EtcPanel : GenericInfoGUI
 
     }
 
-    public override void TaskOnClick()
+    public override void PanelTaskOnClick()
     {
         this.rootGUI.UpdateState(PokeInfoGUI.FSM.Etc);
     }
--- Assets/Scripts/Pokemon/Attributes/SpecificAttributes.cs
+++ Assets/Scripts/Pokemon/Attributes/SpecificAttributes.cs
@@ -19,6 +19,13 @@ public class SpecificAttributes
 
     public SpecificAttributes(int _baseLvl, int _natureID, BaseAttributes _baseStats, BaseAttributes _ivs, BaseAttributes _trainedEVs, int _currentHP)
     {
+        this.currentHP = _currentHP;
+
+        this.CalculateAttributes(_baseLvl, _natureID, _baseStats, _ivs, _trainedEVs);
+    }
+
+    public void CalculateAttributes(int _baseLvl, int _natureID, BaseAttributes _baseStats, BaseAttributes _ivs, BaseAttributes _trainedEVs)
+    {
         Nature nature = SystemManager.Instance.PokemonData.pokemonNatures.RetrievePokemonNature(_natureID);
 
         /**
@@ -26,22 +33,21 @@ public class SpecificAttributes
          * ((IV + 2 * BaseStat + (EV/4)) * Level/100) + 10 + Level
          */
 
-        this.maxHP      = Convert.ToInt32(Math.Floor(((_ivs.hp + (2 * _baseStats.hp) + (_trainedEVs.hp / 4)) * (_baseLvl / 100.0f)))) + 10 + _baseLvl;
-        this.currentHP  = _currentHP;
+        this.maxHP = Convert.ToInt32(Math.Floor(((_ivs.hp + (2 * _baseStats.hp) + (_trainedEVs.hp / 4)) * (_baseLvl / 100.0f)))) + 10 + _baseLvl;
 
         /**
          * For Attack, Defense, Speed, Sp. Attack, Sp. Defense:
          * (((IV + 2 * BaseStat + (EV/4)) * Level/100) + 5) * Nature Value
          */
-        this.attack     = Convert.ToInt32(Math.Floor(((_ivs.attack + (2 * _baseStats.attack) + (_trainedEVs.attack / 4)) * (_baseLvl / 100.0f)))) + 5;
-        this.defense    = Convert.ToInt32(Math.Floor(((_ivs.defense + (2 * _baseStats.defense) + (_trainedEVs.defense / 4)) * (_baseLvl / 100.0f)))) + 5;
-        this.spAttack   = Convert.ToInt32(Math.Floor(((_ivs.spAttack + (2 * _baseStats.spAttack) + (_trainedEVs.spAttack / 4)) * (_baseLvl / 100.0f)))) + 5;
-        this.spDefense  = Convert.ToInt32(Math.Floor(((_ivs.spDefense + (2 * _baseStats.spDefense) + (_trainedEVs.spDefense / 4)) * (_baseLvl / 100.0f)))) + 5;
-        this.speed      = Convert.ToInt32(Math.Floor(((_ivs.speed + (2 * _baseStats.speed) + (_trainedEVs.speed / 4)) * (_baseLvl / 100.0f)))) + 5;
+        this.attack = Convert.ToInt32(Math.Floor(((_ivs.attack + (2 * _baseStats.attack) + (_trainedEVs.attack / 4)) * (_baseLvl / 100.0f)))) + 5;
+        this.defense = Convert.ToInt32(Math.Floor(((_ivs.defense + (2 * _baseStats.defense) + (_trainedEVs.defense / 4)) * (_baseLvl / 100.0f)))) + 5;
+        this.spAttack = Convert.ToInt32(Math.Floor(((_ivs.spAttack + (2 * _baseStats.spAttack) + (_trainedEVs.spAttack / 4)) * (_baseLvl / 100.0f)))) + 5;
+        this.spDefense = Convert.ToInt32(Math.Floor(((_ivs.spDefense + (2 * _baseStats.spDefense) + (_trainedEVs.spDefense / 4)) * (_baseLvl / 100.0f)))) + 5;
+        this.speed = Convert.ToInt32(Math.Floor(((_ivs.speed + (2 * _baseStats.speed) + (_trainedEVs.speed / 4)) * (_baseLvl / 100.0f)))) + 5;
 
-        if(nature.increased_stat_id != nature.decreased_stat_id)
+        if (nature.increased_stat_id != nature.decreased_stat_id)
         {
-            switch(nature.increased_stat_id)
+            switch (nature.increased_stat_id)
             {
                 case (int)PokemonEnums.Stats.attack:
                     this.attack = Convert.ToInt32(Math.Floor(this.attack * 1.1f));
@@ -50,15 +56,15 @@ public class SpecificAttributes
                 case (int)PokemonEnums.Stats.defense:
                     this.defense = Convert.ToInt32(Math.Floor(this.defense * 1.1f));
                     break;
-                
+
                 case (int)PokemonEnums.Stats.spAttack:
                     this.spAttack = Convert.ToInt32(Math.Floor(this.spAttack * 1.1f));
                     break;
-                
+
                 case (int)PokemonEnums.Stats.spDefense:
                     this.spDefense = Convert.ToInt32(Math.Floor(this.spDefense * 1.1f));
                     break;
-                
+
                 case (int)PokemonEnums.Stats.speed:
                     this.speed = Convert.ToInt32(Math.Floor(this.speed * 1.1f));
                     break;
